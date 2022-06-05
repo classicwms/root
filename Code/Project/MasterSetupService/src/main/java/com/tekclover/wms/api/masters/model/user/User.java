9ffd840dd76c7b85194ceeb45fa6677da6cd565a
Murugavel1978 <76118611+Murugavel1978@users.noreply.github.com>
2022-06-05 11:55:32 +0530

    Bakend fixes

--- Code/Project/MasterSetupService/src/main/java/com/tekclover/wms/api/masters/model/user/User.java
+++ Code/Project/MasterSetupService/src/main/java/com/tekclover/wms/api/masters/model/user/User.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -22,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Table(name = "tbluser")
 @JsonInclude(Include.NON_NULL)
 public class User {
 
