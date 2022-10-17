84763bc73b2bbcad058cae8ac260ffd45e206220
Murugavel1978 <76118611+Murugavel1978@users.noreply.github.com>
2022-10-17 09:30:33 +0530

    New changes

--- Code/Project/TransactionService/src/main/java/com/tekclover/wms/api/transaction/repository/MongoInboundRepository.java
+++ Code/Project/TransactionService/src/main/java/com/tekclover/wms/api/transaction/repository/MongoInboundRepository.java
@@ -2,13 +2,12 @@ package com.tekclover.wms.api.transaction.repository;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tekclover.wms.api.transaction.model.inbound.preinbound.InboundIntegrationHeader;
 
 @Repository
-public interface MongoInboundRepository extends MongoRepository<InboundIntegrationHeader, String> {
+public interface MongoInboundRepository { //extends MongoRepository<InboundIntegrationHeader, String> {
 	
 	public List<InboundIntegrationHeader> findTopByProcessedStatusIdOrderByOrderReceivedOn(Long processedStatusId);
 	
--- Code/Project/TransactionService/src/main/java/com/tekclover/wms/api/transaction/repository/MongoOutboundRepository.java
+++ Code/Project/TransactionService/src/main/java/com/tekclover/wms/api/transaction/repository/MongoOutboundRepository.java
@@ -2,13 +2,12 @@ package com.tekclover.wms.api.transaction.repository;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tekclover.wms.api.transaction.model.outbound.preoutbound.OutboundIntegrationHeader;
 
 @Repository
-public interface MongoOutboundRepository extends MongoRepository<OutboundIntegrationHeader, String> {
+public interface MongoOutboundRepository { //extends MongoRepository<OutboundIntegrationHeader, String> {
 	
 	public List<OutboundIntegrationHeader> findTopByProcessedStatusIdOrderByOrderReceivedOn(Long processedStatusId);
 	
