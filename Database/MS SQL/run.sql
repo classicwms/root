5258b6b10f015ea9beeede4cda8fc6795006019b
Murugavel1978 <76118611+Murugavel1978@users.noreply.github.com>
2023-07-15 16:01:33 +0530

    WMS changes

--- Database/MS SQL/run.sql
+++ Database/MS SQL/run.sql
@@ -1,45 +1,152 @@
 USE WMS
 GO
-CREATE TABLE tblimbasicdata1 (
-  LANG_ID NVARCHAR(5) NOT NULL,
-  C_ID NVARCHAR(50) NOT NULL,
-  PLANT_ID NVARCHAR(50) NOT NULL,
-  WH_ID NVARCHAR(50) NOT NULL,
-  ITM_CODE NVARCHAR(500) NOT NULL, 
-  UOM_ID NVARCHAR(10) NOT NULL,
-  TEXT NVARCHAR(500) NULL,
-  Model NVARCHAR(500) NULL,
-  SPEC_01 NVARCHAR(200) NULL,
-  SPEC_02 NVARCHAR(200) NULL,
-  EAN_UPC_NO NVARCHAR(200) NULL,
-  MFR_PART NVARCHAR(200) NULL,
-  HSN_CODE NVARCHAR(50) NULL,  
-  ITM_TYP_ID INTEGER NULL,
-  ITM_GRP_ID INTEGER NULL,
-  SUB_ITM_GRP_ID INTEGER NULL,
-  ST_SEC_ID NVARCHAR(10) NULL,
-  TOT_STK DECIMAL(30,4) NULL,
-  MIN_STK DECIMAL(30,4) NULL,
-  MAX_STK DECIMAL(30,4) NULL,  
-  RE_ORD_LVL DECIMAL(30,4) NULL,
-  REP_QTY DECIMAL(30,4) NULL,
-  SAFTY_STCK DECIMAL(30,4) NULL,
-  STATUS_ID INTEGER NULL,
-  REF_FIELD_1 NVARCHAR(200) NULL,
-  REF_FIELD_2 NVARCHAR(200) NULL,
-  REF_FIELD_3 NVARCHAR(200) NULL,
-  REF_FIELD_4 NVARCHAR(200) NULL,
-  REF_FIELD_5 NVARCHAR(200) NULL,
-  REF_FIELD_6 NVARCHAR(200) NULL,
-  REF_FIELD_7 NVARCHAR(200) NULL,
-  REF_FIELD_8 NVARCHAR(200) NULL,
-  REF_FIELD_9 NVARCHAR(200) NULL,
-  REF_FIELD_10 NVARCHAR(200) NULL,
-  IS_DELETED INTEGER NULL,
-  CTD_BY NVARCHAR(50) NULL,
-  CTD_ON DATETIME NULL,
-  UTD_BY NVARCHAR(50) NULL,
-  UTD_ON DATETIME NULL,
-  
-  PRIMARY KEY ( C_ID, PLANT_ID, WH_ID, ITM_CODE, LANG_ID, UOM_ID))
-COMMENT = 'IM BASIC DATA1';
+update tblputawayline set itm_code='009453779' where pack_barcode='1060000915';
+Go
+update tblputawayline set itm_code='009452787' where pack_barcode='1060000928';
+Go
+update tblputawayline set itm_code='009454050' where pack_barcode='1060000930';
+Go
+update tblputawayline set itm_code='009454565' where pack_barcode='1060000931';
+Go
+update tblputawayline set itm_code='009454581' where pack_barcode='1060000933';
+Go
+update tblputawayline set itm_code='009451046' where pack_barcode='1060000935';
+Go
+update tblputawayline set itm_code='009454565' where pack_barcode='1060000936';
+Go
+update tblputawayline set itm_code='009454565' where pack_barcode='1060000937';
+Go
+update tblputawayline set itm_code='009454590' where pack_barcode='1060000938';
+Go
+update tblputawayline set itm_code='009454056' where pack_barcode='1060000939';
+Go
+update tblputawayline set itm_code='009454056' where pack_barcode='1060000940';
+Go
+update tblputawayline set itm_code='009454590' where pack_barcode='1060000941';
+Go
+update tblputawayline set itm_code='009451247' where pack_barcode='1060000942';
+Go
+update tblputawayline set itm_code='009451247' where pack_barcode='1060000943';
+Go
+update tblputawayline set itm_code='009453395' where pack_barcode='1060000944';
+Go
+update tblputawayline set itm_code='009452781' where pack_barcode='1060000945';
+Go
+update tblputawayline set itm_code='009454010' where pack_barcode='1060000947';
+Go
+update tblputawayline set itm_code='009451432' where pack_barcode='1060000951';
+Go
+update tblputawayline set itm_code='009451426' where pack_barcode='1060000955';
+Go
+update tblputawayline set itm_code='009452769' where pack_barcode='1060000961';
+Go
+update tblputawayline set itm_code='009452769' where pack_barcode='1060000962';
+Go
+update tblputawayline set itm_code='21309997562' where pack_barcode='1060000991';
+Go
+update tblputawayline set itm_code='21309997465' where pack_barcode='1060000996';
+Go
+update tblputawayline set itm_code='21309997478' where pack_barcode='1060000998';
+Go
+update tblputawayline set itm_code='177220' where pack_barcode='1060001004';
+Go
+update tblputawayline set itm_code='027861043' where pack_barcode='1060001013';
+Go
+update tblputawayline set itm_code='027861042' where pack_barcode='1060001014';
+Go
+update tblputawayline set itm_code='0203013996' where pack_barcode='1060001021';
+Go
+update tblputawayline set itm_code='0057517244' where pack_barcode='1060002557';
+Go
+update tblputawayline set itm_code='0057517293' where pack_barcode='1060002577';
+Go
+update tblputawayline set itm_code='0057511951' where pack_barcode='1060002590';
+Go
+update tblputawayline set itm_code='0057511953' where pack_barcode='1060002625';
+Go
+update tblputawayline set itm_code='0057511950' where pack_barcode='1060002629';
+Go
+update tblputawayline set itm_code='0057514174' where pack_barcode='1060002637';
+Go
+update tblputawayline set itm_code='020303325' where pack_barcode='1060003273';
+Go
+update tblputawayline set itm_code='020303325' where pack_barcode='1060003299';
+Go
+update tblputawayline set itm_code='020303325' where pack_barcode='1060003300';
+Go
+update tblputawayline set itm_code='020303325' where pack_barcode='1060003301';
+Go
+update tblputawayline set itm_code='020303325' where pack_barcode='1060003302';
+Go
+update tblputawayline set itm_code='020303585' where pack_barcode='1060003414';
+Go
+update tblputawayline set itm_code='020303585' where pack_barcode='1060003415';
+Go
+update tblputawayline set itm_code='020303585' where pack_barcode='1060003416';
+Go
+update tblputawayline set itm_code='020303585' where pack_barcode='1060003417';
+Go
+update tblputawayline set itm_code='020303585' where pack_barcode='1060003433';
+Go
+update tblputawayline set itm_code='020303811' where pack_barcode='1060003434';
+Go
+update tblputawayline set itm_code='020303811' where pack_barcode='1060003479';
+Go
+update tblputawayline set itm_code='020303811' where pack_barcode='1060003480';
+Go
+update tblputawayline set itm_code='020303811' where pack_barcode='1060003481';
+Go
+update tblputawayline set itm_code='009452769' where pack_barcode='1060005846';
+Go
+update tblputawayline set itm_code='009454565' where pack_barcode='1060005854';
+Go
+update tblputawayline set itm_code='020302547' where pack_barcode='1060018146';
+Go
+update tblputawayline set itm_code='020302547' where pack_barcode='1060018164';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021105';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021106';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021107';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021108';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021109';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021110';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021111';
+Go
+update tblputawayline set itm_code='0203011055' where pack_barcode='1060021112';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021113';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021114';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021115';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021116';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021117';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021118';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021119';
+Go
+update tblputawayline set itm_code='0203011056' where pack_barcode='1060021120';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021121';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021122';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021123';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021124';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021125';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021126';
+Go
+update tblputawayline set itm_code='0203014804' where pack_barcode='1060021127';
+Go
