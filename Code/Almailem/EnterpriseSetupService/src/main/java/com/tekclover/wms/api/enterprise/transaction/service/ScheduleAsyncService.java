e613646d0f4ac07e353ac729984a774b552dc1b2
iwexpress <76118611+Murugavel1978@users.noreply.github.com>
2025-01-30 14:12:43 +0530

    AMG changes

--- Code/Almailem/EnterpriseSetupService/src/main/java/com/tekclover/wms/api/enterprise/transaction/service/ScheduleAsyncService.java
+++ Code/Almailem/EnterpriseSetupService/src/main/java/com/tekclover/wms/api/enterprise/transaction/service/ScheduleAsyncService.java
@@ -1,14 +1,16 @@
 package com.tekclover.wms.api.enterprise.transaction.service;
 
-import com.tekclover.wms.api.enterprise.transaction.model.warehouse.inbound.WarehouseApiResponse;
-import lombok.extern.slf4j.Slf4j;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.InvocationTargetException;
-import java.text.ParseException;
-import java.util.concurrent.CompletableFuture;
+import com.tekclover.wms.api.enterprise.transaction.model.warehouse.inbound.WarehouseApiResponse;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
---
+++ Code/Amghara/EnterpriseSetupService/src/main/java/com/tekclover/wms/api/enterprise/transaction/scheduler/BatchJobScheduler.java
@@ -0,0 +1,34 @@
+package com.tekclover.wms.api.enterprise.transaction.scheduler;
+
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.tekclover.wms.api.enterprise.transaction.model.warehouse.inbound.WarehouseApiResponse;
+import com.tekclover.wms.api.enterprise.transaction.service.ScheduleAsyncService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class BatchJobScheduler {
+
+    @Autowired
+    ScheduleAsyncService scheduleAsyncService;
+
+    //-------------------------------------------------------------------------------------------
+
+    @Scheduled(fixedDelay = 20000)
+    public void scheduleJob() throws InterruptedException, InvocationTargetException, IllegalAccessException, ParseException {
+//        CompletableFuture<WarehouseApiResponse> outboundOrder = scheduleAsyncService.processOutboundOrder();
+        CompletableFuture<WarehouseApiResponse> amgharaOutboundOrder = scheduleAsyncService.processAmgharaOutboundOrder();
+        CompletableFuture<WarehouseApiResponse> outboundFailedOrder = scheduleAsyncService.processOutboundFailedOrder();
+
+//        CompletableFuture.allOf(inboundOrder,outboundOrder,perpetualStockCountOrder,periodicStockCountOrder,stockAdjustmentOrder,inboundFailedOrder,outboundFailedOrder).join();
+    }
+
+}
