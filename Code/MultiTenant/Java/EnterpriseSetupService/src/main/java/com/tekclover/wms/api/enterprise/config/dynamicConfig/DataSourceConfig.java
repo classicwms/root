10078ef568018d2c1959792fed57780f29af8219
M-C-Bharathi <bharathi.m@tekclover.com>
2025-08-19 10:11:47 +0530

    multi

---
+++ Code/MultiTenant/Java/EnterpriseSetupService/src/main/java/com/tekclover/wms/api/enterprise/config/dynamicConfig/DataSourceConfig.java
@@ -0,0 +1,165 @@
+package com.tekclover.wms.api.enterprise.config.dynamicConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.tekclover.wms.api.enterprise",entityManagerFactoryRef = "entityManager", transactionManagerRef = "multiTransactionManager")
+public class DataSourceConfig {
+
+    @Bean
+    public DynamicDataSource dataSource() {
+        // Setup the target data sources (you can dynamically load these from a DB or config)
+        Map<Object, Object> targetDataSources = new HashMap<>();
+
+        // Example data source configurations (replace with actual configuration)
+        DataSource db = createDataSource("MT");
+        DataSource db1 = createDataSource("IMPEX");
+        DataSource db2 = createDataSource("WK");
+        DataSource db3 = createDataSource("ALM");
+        DataSource db4 = createDataSource("NAMRATHA");
+        DataSource db5 = createDataSource("REEFERON");
+        DataSource db6 = createDataSource("KNOWELL");
+        DataSource db7 = createDataSource("FAHAHEEL");
+        DataSource db8 = createDataSource("AUTO_LAP");
+        DataSource db9 = createDataSource("BP");
+        DataSource db10 = createDataSource("JAHRA");
+
+
+        targetDataSources.put("MT",db);
+        targetDataSources.put("IMPEX", db1);
+        targetDataSources.put("WK", db2);
+        targetDataSources.put("ALM",db3);
+        targetDataSources.put("NAMRATHA",db4);
+        targetDataSources.put("REEFERON",db5);
+        targetDataSources.put("KNOWELL", db6);
+        targetDataSources.put("FAHAHEEL", db7);
+        targetDataSources.put("AUTO_LAP", db8);
+        targetDataSources.put("BP", db9);
+        targetDataSources.put("JAHRA", db10);
+
+        DynamicDataSource dataSource = new DynamicDataSource();
+        dataSource.setTargetDataSources(targetDataSources);
+        dataSource.setDefaultTargetDataSource(db); // Default database (e.g., db1)
+        return dataSource;
+    }
+
+    private DataSource createDataSource(String dbName) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // Configure database connection here
+        switch (dbName) {
+            case "MT":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=wms_imf");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "ALM":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_ALMDEV_SPLIT");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "NAMRATHA":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.14.33;databaseName=WMS_NAMRATHA");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("VcDBpRrebn2N8LgBPSYitDcsbV");
+                break;
+            case "IMPEX":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_IMPEX");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "WK":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_WK");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "REEFERON":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=REEFERON_DEV");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "KNOWELL":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_KNOWELL_DEV");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "FAHAHEEL":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_FAHAHEEL");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "AUTO_LAP":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_AUTO_LAP");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "JAHRA":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.22.24;databaseName=WMS_JAHRA");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("9SOwjgFjm0sM7qMOFz16mICJUx");
+                break;
+            case "BP":
+                dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                dataSource.setUrl("jdbc:sqlserver://10.10.14.33;databaseName=WMS_BP");
+                dataSource.setUsername("sa");
+                dataSource.setPassword("VcDBpRrebn2N8LgBPSYitDcsbV");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown database: " + dbName);
+        }
+        return dataSource;
+    }
+    @Bean(name = "entityManager")
+    public LocalContainerEntityManagerFactoryBean entityManager() throws PropertyVetoException {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setPackagesToScan("com.tekclover.wms.api.enterprise.model");
+        entityManagerFactory.setJpaProperties(hibernateProperties());
+        return entityManagerFactory;
+    }
+
+    @Bean(name = "multiTransactionManager")
+    public PlatformTransactionManager multiTransactionManager() throws PropertyVetoException {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManager().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.SQLServer2012Dialect");  // Adjust for your DB
+        properties.put("hibernate.hbm2ddl.auto", "update");  // Or "validate" for production
+        properties.put("hibernate.show_sql", "false");  // Logs the generated SQL
+        properties.put("spring.jpa.hibernate.ddl-auto", "update");  // Logs the generated SQL
+        properties.put("hibernate.format_sql", "false");  // Pretty formats the SQL
+        properties.put("hibernate.use_sql_comments", "false");  // Adds comments to SQL for better debugging
+        return properties;
+    }
+
+}
---
+++ Code/MultiTenant/Java/EnterpriseSetupService/src/main/java/com/tekclover/wms/api/enterprise/controller/StrategiesController.java
@@ -0,0 +1,153 @@
+package com.tekclover.wms.api.enterprise.controller;
+
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.tekclover.wms.api.enterprise.config.dynamicConfig.DataBaseContextHolder;
+import com.tekclover.wms.api.enterprise.repository.DbConfigRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tekclover.wms.api.enterprise.model.strategies.AddStrategies;
+import com.tekclover.wms.api.enterprise.model.strategies.Strategies;
+import com.tekclover.wms.api.enterprise.model.strategies.SearchStrategies;
+import com.tekclover.wms.api.enterprise.model.strategies.UpdateStrategies;
+import com.tekclover.wms.api.enterprise.service.StrategiesService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Validated
+@Api(tags = {"Strategies "}, value = "Strategies  Operations related to StrategiesController") // label for swagger
+@SwaggerDefinition(tags = {@Tag(name = "Strategies ",description = "Operations related to Strategies ")})
+@RequestMapping("/strategies")
+@RestController
+public class StrategiesController {
+	
+	@Autowired
+	StrategiesService strategiesService;
+
+	@Autowired
+	DbConfigRepository dbConfigRepository;
+	
+    @ApiOperation(response = Strategies.class, value = "Get all Strategies details") // label for swagger
+	@GetMapping("")
+	public ResponseEntity<?> getAll() {
+		List<Strategies> strategiesList = strategiesService.getStrategiess();
+		return new ResponseEntity<>(strategiesList, HttpStatus.OK); 
+	}
+    
+	@ApiOperation(response = Strategies.class, value = "Get a Strategies") 
+	@GetMapping("/strategies")
+	public ResponseEntity<?> getStrategies(@RequestParam String warehouseId,@RequestParam String companyId,
+										   @RequestParam String languageId,@RequestParam String plantId) {
+		try {
+			DataBaseContextHolder.setCurrentDb("MT");
+			String routingDb = dbConfigRepository.getDbName(companyId,plantId, warehouseId);
+			log.info("ROUTING DB FETCH FROM DB CONFIG TABLE --> {}", routingDb);
+			DataBaseContextHolder.clear();
+			DataBaseContextHolder.setCurrentDb(routingDb);
+	   	List<Strategies> strategies = strategiesService.getStrategies(companyId,languageId,plantId,warehouseId);
+	   	log.info("Strategies : " + strategies);
+		return new ResponseEntity<>(strategies, HttpStatus.OK);
+	}
+		finally {
+			DataBaseContextHolder.clear();
+		}
+		}
+    
+    @ApiOperation(response = Strategies.class, value = "Search Strategies") // label for swagger
+	@PostMapping("/findStrategies")
+	public List<Strategies> findStrategies(@RequestBody SearchStrategies searchStrategies)
+			throws Exception {
+		try {
+			DataBaseContextHolder.setCurrentDb("MT");
+			String routingDb = dbConfigRepository.getDbName(searchStrategies.getCompanyId(), searchStrategies.getPlantId(), searchStrategies.getWarehouseId());
+			log.info("ROUTING DB FETCH FROM DB CONFIG TABLE --> {}", routingDb);
+			DataBaseContextHolder.clear();
+			DataBaseContextHolder.setCurrentDb(routingDb);
+		return strategiesService.findStrategies(searchStrategies);
+	}
+		finally {
+			DataBaseContextHolder.clear();
+		}
+		}
+    
+    @ApiOperation(response = Strategies.class, value = "Create Strategies") // label for swagger
+	@PostMapping("")
+	public ResponseEntity<?> postStrategies(@Valid @RequestBody List<AddStrategies> newStrategies, @RequestParam String loginUserID)
+			throws IllegalAccessException, InvocationTargetException, ParseException {
+		try {
+			for (AddStrategies addStrategies : newStrategies){
+			DataBaseContextHolder.setCurrentDb("MT");
+			String routingDb = dbConfigRepository.getDbName(addStrategies.getCompanyId(), addStrategies.getPlantId(), addStrategies.getWarehouseId());
+			log.info("ROUTING DB FETCH FROM DB CONFIG TABLE --> {}", routingDb);
+			DataBaseContextHolder.clear();
+			DataBaseContextHolder.setCurrentDb(routingDb);
+		}
+		List<Strategies> createdStrategies = strategiesService.createStrategies(newStrategies, loginUserID);
+		return new ResponseEntity<>(createdStrategies , HttpStatus.OK);
+	}
+		finally {
+			DataBaseContextHolder.clear();
+		}
+		}
+    
+    @ApiOperation(response = Strategies.class, value = "Update Strategies") // label for swagger
+    @PatchMapping("/strategies")
+	public ResponseEntity<?> patchStrategies( @RequestParam String warehouseId,@RequestParam String companyId,
+											 @RequestParam String languageId, @RequestParam String plantId,
+											 @Valid @RequestBody List<AddStrategies> updateStrategies,
+											 @RequestParam String loginUserID)
+			throws IllegalAccessException, InvocationTargetException, ParseException {
+		try {
+			DataBaseContextHolder.setCurrentDb("MT");
+			String routingDb = dbConfigRepository.getDbName(companyId,plantId, warehouseId);
+			log.info("ROUTING DB FETCH FROM DB CONFIG TABLE --> {}", routingDb);
+			DataBaseContextHolder.clear();
+			DataBaseContextHolder.setCurrentDb(routingDb);
+		List<Strategies> createdStrategies = strategiesService.updateStrategies(companyId,languageId,plantId,warehouseId,updateStrategies, loginUserID);
+		return new ResponseEntity<>(createdStrategies , HttpStatus.OK);
+	}
+		finally {
+			DataBaseContextHolder.clear();
+		}
+		}
+    
+    @ApiOperation(response = Strategies.class, value = "Delete Strategies") // label for swagger
+	@DeleteMapping("/strategies")
+	public ResponseEntity<?> deleteStrategies(@RequestParam String companyId,
+											  @RequestParam String languageId,@RequestParam String plantId,
+											  @RequestParam String warehouseId,@RequestParam String loginUserID) throws ParseException {
+		try {
+			DataBaseContextHolder.setCurrentDb("MT");
+			String routingDb = dbConfigRepository.getDbName(companyId,plantId, warehouseId);
+			log.info("ROUTING DB FETCH FROM DB CONFIG TABLE --> {}", routingDb);
+			DataBaseContextHolder.clear();
+			DataBaseContextHolder.setCurrentDb(routingDb);
+		strategiesService.deleteStrategies(companyId,languageId,plantId,warehouseId,loginUserID);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+		finally {
+			DataBaseContextHolder.clear();
+		}
+		}
+}
