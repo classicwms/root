9c7b40d4a307eb0bb71c09242b29eae1a6326603
M-C-Bharathi <bharathi.m@tekclover.com>
2025-09-11 19:43:35 +0530

    wl

--- Walkaroo/Prod_V2/08_09_2025/TransactionService/src/main/java/com/tekclover/wms/api/transaction/config/AsyncConfiguration.java
+++ Walkaroo/Prod_V2/08_09_2025/TransactionService/src/main/java/com/tekclover/wms/api/transaction/config/AsyncConfiguration.java
@@ -11,41 +11,41 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableAsync
 public class AsyncConfiguration {
-//    @Bean(name = "asyncExecutor")
-//    public Executor asyncExecutor() {
-////        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-////        taskExecutor.setCorePoolSize(5);
-////        taskExecutor.setQueueCapacity(100);
-////        taskExecutor.setMaxPoolSize(5);
-////        taskExecutor.setThreadNamePrefix("AsyncTaskThread-");
-////        taskExecutor.initialize();
-////        return taskExecutor;
-//
-//        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-//        threadPoolTaskExecutor.setThreadNamePrefix("AsyncTaskThread-");
-//        threadPoolTaskExecutor.setCorePoolSize(10);
-//        threadPoolTaskExecutor.setMaxPoolSize(30);
-//        threadPoolTaskExecutor.setKeepAliveSeconds(60);
-//        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-//        threadPoolTaskExecutor.setQueueCapacity(100);
-//        threadPoolTaskExecutor.setRejectedExecutionHandler(new MyTaskExecutionHandlerImpl());
-//        threadPoolTaskExecutor.initialize();
-//        return threadPoolTaskExecutor;
-//    }
-
     @Bean(name = "asyncExecutor")
     public Executor asyncExecutor() {
-        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-        taskExecutor.setCorePoolSize(5);   // Minimum threads
-        taskExecutor.setMaxPoolSize(10);   // Maximum threads
-        taskExecutor.setQueueCapacity(100);// Queue size
-        taskExecutor.setThreadNamePrefix("AsyncTaskThread-");
+//        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//        taskExecutor.setCorePoolSize(5);
+//        taskExecutor.setQueueCapacity(100);
+//        taskExecutor.setMaxPoolSize(5);
+//        taskExecutor.setThreadNamePrefix("AsyncTaskThread-");
+//        taskExecutor.initialize();
+//        return taskExecutor;
 
-        // Handle rejected tasks (when pool is full)
-        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-
-        taskExecutor.initialize();
-        return taskExecutor;
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("AsyncTaskThread-");
+        threadPoolTaskExecutor.setCorePoolSize(10);
+        threadPoolTaskExecutor.setMaxPoolSize(30);
+        threadPoolTaskExecutor.setKeepAliveSeconds(60);
+        threadPoolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        threadPoolTaskExecutor.setQueueCapacity(100);
+        threadPoolTaskExecutor.setRejectedExecutionHandler(new MyTaskExecutionHandlerImpl());
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
     }
+
+//    @Bean(name = "asyncExecutor")
+//    public Executor asyncExecutor() {
+//        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//        taskExecutor.setCorePoolSize(5);   // Minimum threads
+//        taskExecutor.setMaxPoolSize(10);   // Maximum threads
+//        taskExecutor.setQueueCapacity(100);// Queue size
+//        taskExecutor.setThreadNamePrefix("AsyncTaskThread-");
+//
+//        // Handle rejected tasks (when pool is full)
+//        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+//
+//        taskExecutor.initialize();
+//        return taskExecutor;
+//    }
 
 }
