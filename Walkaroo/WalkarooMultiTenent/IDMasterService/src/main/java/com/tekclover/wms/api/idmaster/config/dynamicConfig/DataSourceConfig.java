9d9af8c9fba3f6ed305620959ead2c57457b35db
M-C-Bharathi <bharathi.m@tekclover.com>
2026-01-19 09:42:30 +0530

    plant

--- Walkaroo/WalkarooMultiTenent/IDMasterService/src/main/java/com/tekclover/wms/api/idmaster/config/dynamicConfig/DataSourceConfig.java
+++ Walkaroo/WalkarooMultiTenent/IDMasterService/src/main/java/com/tekclover/wms/api/idmaster/config/dynamicConfig/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package com.tekclover.wms.api.idmaster.config.dynamicConfig;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
