2a546102150d918af961c8fce7b47a3b82e14b39
M-C-Bharathi <bharathi.m@tekclover.com>
2026-04-02 08:09:48 +0530

    preoutbound_spark

---
+++ Walkaroo/WalkarooMultiTenent/WrapperService/src/main/java/com/tekclover/wms/core/model/spark/NewPickupHeaderRes.java
@@ -0,0 +1,22 @@
+package com.tekclover.wms.core.model.spark;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class NewPickupHeaderRes {
+
+    private String companyCodeId;
+    private String plantId;
+    private String warehouseId;
+    private String customerId;
+    private String customerName;
+    private String pickListNumber;
+    private String shipToCode;
+    private String shipToParty;
+    private Timestamp pickupCreatedOn;
+
+    private String languageId;
+    private String pickupNumber;
+}
