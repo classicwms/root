a09af477bbca0a2bbe93c34a1919ec2d925aed44
M-C-Bharathi <bharathi.m@tekclover.com>
2026-09-21 16:56:19 +0530

    amg_prod

--- Code/Amghara/TransactionService/src/main/java/com/tekclover/wms/api/transaction/config/kafka/KafkaConsumerConfig.java
+++ Code/Amghara/TransactionService/src/main/java/com/tekclover/wms/api/transaction/config/kafka/KafkaConsumerConfig.java
@@ -62,7 +62,7 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, QualityLineCreateEvent> qualityLineProcessConsumerFactory() {
-        return createConsumerFactory(QualityLineCreateEvent.class, "qualityline-create-topic-v3");
+        return createConsumerFactory(QualityLineCreateEvent.class, "qualityline-create-topic-v4");
     }
 
     @Bean
